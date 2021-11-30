5924d14aaa828f181cc81d99199d4ffe72cf7619
Yena Choi <yena@deali.net>
2021-11-30 17:54:15 +0900

    Week 2 Init, Codelab 6-5까지 완료

--- week 2-1-Layouts in JetpackCompose/LayoutCompose/app/src/main/java/com/example/layoutcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutCompose/app/src/main/java/com/example/layoutcompose/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.layoutcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.layoutcompose.ui.theme.LayoutComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,49 +25,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutComposeTheme {
-                LayoutsCodeLab()
+                ImageList()
             }
         }
     }
 }
 
 @Composable
-fun LayoutsCodeLab() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "LayoutsCodeLab")
-                },
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(Icons.Filled.Favorite, contentDescription = null)
-                        
-                    }
-                }
-            )
-        }
+fun ImageList() {
+    val scrollState = rememberLazyListState()
 
-
-    ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding))
+    LazyColumn(state = scrollState) {
+        items(100) { ImageListItem(index = it)}
     }
 }
+
+
 
 @Composable
-fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item $index", style = MaterialTheme.typography.subtitle1)
     }
+
 }
+
 
 @Preview
 @Composable
-fun LayoutsCodeLabPreview() {
+fun ImageListPreview() {
     LayoutComposeTheme {
-        LayoutsCodeLab()
+        ImageList()
+
     }
 }
-
-
